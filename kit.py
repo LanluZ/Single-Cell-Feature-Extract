66be212c5596490cad31af3442502a7032186159
LanluZ <LanluZ@qq.com>
2024-04-20 19:04:04 +0800

    1.修复内接圆计算bug

--- kit.py
+++ kit.py
@@ -13,26 +13,26 @@ import datetime
 def calCircleIn(img, contours_arr):
     # 计算到轮廓的距离
     raw_dist = np.empty((img.shape[0], img.shape[1]), dtype=np.float32)
-    start_i = contours_arr[0, 0, 1]  # 起始行
-    end_i = contours_arr[-1, 0, 1] + 1  # 结束行
-    for i in range(start_i, end_i):
-        for j in range(img.shape[1]):
+    start_i, end_i = np.min(contours_arr[:, 0, 1]), np.max(contours_arr[:, 0, 1])
+    start_j, end_j = np.min(contours_arr[:, 0, 0]), np.max(contours_arr[:, 0, 0])
+    for i in range(start_i, end_i + 1):
+        for j in range(start_j, end_j + 1):
             raw_dist[i, j] = cv2.pointPolygonTest(contours_arr, (j, i), True)
 
     # 获取最大值即内接圆半径，中心点坐标
-    _min_val, max_val, _, _max_dist_pt = cv2.minMaxLoc(raw_dist)
+    min_val, max_val, _, max_dist_pt = cv2.minMaxLoc(raw_dist)
+    min_val = abs(min_val)
     max_val = abs(max_val)
-    radius = np.int_(max_val)
 
-    return radius, _max_dist_pt  # 返回半径和中心点坐标
+    return max_val, max_dist_pt  # 返回半径和中心点坐标
 
 
 # 内接圆绘制
 def drawCircleIn(filename, save_path, img, contours_arr):  # 画出最大内接圆 避免出事
     img = copy.copy(img)  # 防止指向同一内存
     radius, center = calCircleIn(img, contours_arr)
-    cv2.circle(img, center, radius, (0, 255, 0), 1)
-    cv2.circle(img, center, 1, (0, 255, 0), 1)
+    cv2.circle(img, center, radius, (0, 0, 255), 1)
+    cv2.circle(img, center, 1, (0, 0, 255), 1)
     cv2.imwrite(os.path.join(save_path, filename), img)
 
     return radius
@@ -52,8 +52,8 @@ def drawCircleOut(filename, save_path, img, contour):
     radius, (x, y) = calCircleOut(contour)
     center = (int(x), int(y))  # 最小内接圆圆心
     radius = int(radius)  # 半径
-    cv2.circle(img, center, radius, (0, 255, 0), 1)
-    cv2.circle(img, center, 1, (0, 255, 0), 1)
+    cv2.circle(img, center, radius, (0, 0, 255), 1)
+    cv2.circle(img, center, 1, (0, 0, 255), 1)
     cv2.imwrite(os.path.join(save_path, filename), img)
 
     return radius
